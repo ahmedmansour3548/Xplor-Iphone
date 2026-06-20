0d60f37dead46bba4bd542530fd2892a665980b4
Ahmed Mansour <120754004+ahmedmansour3548@users.noreply.github.com>
2026-06-20 16:45:05 +0900

    Initial Commit

---
+++ Xplor/XplorApp.swift
@@ -0,0 +1,17 @@
+//
+//  XplorApp.swift
+//  Xplor
+//
+//  Created by Ahmed Mansour on 6/20/26.
+//
+
+import SwiftUI
+
+@main
+struct XplorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
