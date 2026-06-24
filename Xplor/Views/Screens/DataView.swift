c90f47eae2c6d83a7f8d57c1d9121a4203ff3370
Ahmed Mansour <120754004+ahmedmansour3548@users.noreply.github.com>
2026-06-24 19:14:07 +0900

    added button to lock camera

---
+++ Xplor/Views/Screens/DataView.swift
@@ -0,0 +1,38 @@
+//
+//  DataView.swift
+//  Xplor
+//
+//  Created by Ahmed Mansour on 6/21/26.
+//
+
+import SwiftUI
+
+struct DataView: View {
+
+    @EnvironmentObject var explorationManager:
+        ExplorationManager
+
+    var body: some View {
+
+        ScrollView {
+
+            VStack(spacing: 20) {
+
+                TileStatsCard(
+                    totalTiles:
+                        explorationManager
+                            .discoveredTiles
+                            .count,
+
+                    todayTiles:
+                        explorationManager
+                            .tilesDiscoveredToday
+                )
+
+                Spacer()
+            }
+            .padding()
+        }
+        .navigationTitle("Data")
+    }
+}
